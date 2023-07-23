5ff5608d148c852aa272d4df92136984486e26bf
Joe Palala <joe@palalamail.xyz>
2023-07-23 10:37:32 +0800

    Update concede.sh

--- concede.sh
+++ concede.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ## Basic gist is 1. Get the previous commit through git rev-parse , reset it (--mixed by default), stash your working dir, (optionally) clean it, git s
-PREV_NUM_COMMITS_BACK=1
+DEFAULT_PREV_NUM=1
 # (optional) first lookup your previous commits and find the commit
 # git log | tail -n 10
- 
-read -p "Are you sure to revert and stash to past -$PREV_NUM_COMMITS_BACK commit back?" -n 1 -r
+echo "revert to $DEFAULT_PREV_NUM ?"
+read -p "Enter your answer (y/n) " -n 1 -r
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
@@ -13,11 +13,15 @@ then
    git reset $PREV_COMMIT
    # uncomment if you want a clean slate (but keep env files from deletion)
    #git clean -e *.env* -fd 
-   T_NOW=date +"%H:%M"
-   git stash save "stashed back HEAD minus $DEFAULT_PREV_NUM changes at $T_NOW"
+   T_NOW=$(date +"%H:%M")
+   git stash save "stashed back HEAD minus $DEFAULT_PREV_NUM changes at $T_NOW \n\n\n"
 else 
   echo "Aborting..."   
 fi
 # TODO ask user what to do next..
-echo "You may now switch branch and apply your stash{0} and commit them manually"
+echo "\n\n"
+echo "You may now switch branch and pull changes from main then apply your stashed changes"
+
+echo "\"Thank you come again\" -Apu"
+
 
