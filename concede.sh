f55e9bbb91d5188a76353723a0d1ffaba6552bdd
Joe Palala <joe@palalamail.xyz>
2023-07-23 10:42:56 +0800

    Update concede.sh

--- concede.sh
+++ concede.sh
@@ -3,13 +3,13 @@
 DEFAULT_PREV_NUM=1
 # (optional) first lookup your previous commits and find the commit
 # git log | tail -n 10
-echo "revert to $DEFAULT_PREV_NUM ?"
+PREV_COMMIT=$(git rev-parse HEAD~$DEFAULT_PREV_NUM)
+echo "revert to $PREV_COMMIT?"
 read -p "Enter your answer (y/n) " -n 1 -r
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-   echo  "We are now going to stash"  # (optional) move to a new line
-   PREV_COMMIT=$(git rev-parse HEAD~$DEFAULT_PREV_NUM)
+   echo  "We are now going reset to the previous commit and stash the changes (into working dir)"  # (optional) move to a new line
    git reset $PREV_COMMIT
    # uncomment if you want a clean slate (but keep env files from deletion)
    #git clean -e *.env* -fd 
